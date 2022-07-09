9ae2746c0ecdeda7b37ce95ead62b9e87af4d0bf
jayant <jayantparmar420@gmail.com>
2022-07-09 14:28:08 +0530

    Added the employee payroll dashboard and model

---
+++ app/components/add/add.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/model/employee';
+import { EmployeeService } from 'src/app/services/employee.service';
+
+@Component({
+  selector: 'app-add',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css']
+})
+export class AddComponent implements OnInit {
+  
+  constructor(private services:EmployeeService) { }
+
+  ngOnInit(): void {
+  }
+
+  formatLabel(value: number) {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+    return value;
+  }
+
+  insert(employee:any) {
+    alert(employee.name);
+    this.services.addEmployee(employee);
+  }
+}
---
+++ app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/model/employee';
+import { EmployeeService } from 'src/app/services/employee.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  employees:Employee[] = []
+
+  constructor(private services:EmployeeService) { }
+
+  ngOnInit(): void {
+    this.employees =  this.services.getEmployees();
+  }
+
+}
